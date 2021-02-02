f5b075f9fd9019e14603fb32852976fd8a611d98
Sagar Giri <sagargiri8846@gmail.com>
2021-02-02 18:54:51 +0530

    bug fixes

--- src/components/join/Join.js
+++ src/components/join/Join.js
@@ -8,8 +8,8 @@ function Joins(props) {
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
                 <h1 className="heading">Join</h1>
-                <div><input placeholdere="Name" value={name} type="text" className="joinInput" onChange={(e)=>setName(e.target.value)} /></div>
-                <div><input placeholdere="Room" vlaue={room} type="text" className="joinInput mt-20" onChange={(e)=>setRoom(e.target.value)} /></div>
+                <div>Name:-<input placeholdere="Name" value={name} type="text" className="joinInput" onChange={(e)=>setName(e.target.value)} /></div>
+                <div>Room Id:<input placeholdere="Room" vlaue={room} type="text" className="joinInput mt-20" onChange={(e)=>setRoom(e.target.value)} /></div>
                 <Link onClick={(event)=>(!name || !room)?event.preventDefault():null} to={`/chat?name=${name}&room=${room}`}>
                     <button className="button mt-20" type="submit">
                         Signin
